006afbad5213b39a2cb7efcb9b06147f7b82ad53
Amanda R <abrolon87@gmail.com>
2019-11-10 17:58:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,12 +23,12 @@ end
 
 # return a hash that includes the key and value parameters passed into this method
 def my_hash_creator(key, value)
-  my_hash_creator = {:name => "Grace Hopper"}
+  my_hash_creator = {key => value}
 end
 
 # return the correct value using the hash and key parameters
 def read_from_hash(hash, key)
-
+  hash[key]
 end
 
 
